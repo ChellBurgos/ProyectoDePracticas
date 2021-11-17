48c7d99817b0d3d6fc53fdca77e459af39c1172d
Alejandro Moraga Gutiérrez <alemoragaG1304@outlook.com>
2021-11-17 16:53:05 -0600

    Ejemplo

--- obj/Release/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Release/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace TiendaArtesaniasMarielos.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 1 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 2 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 3 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 4 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 5 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 6 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 7 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 8 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 9 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using TiendaArtesaniasMarielos;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\VisualStudio\ProyectoDePracticas\_Imports.razor"
+#line 10 "C:\Users\Alejandro Moraga\Source\Repos\ChellBurgos\ProyectoDePracticas\_Imports.razor"
 using TiendaArtesaniasMarielos.Shared;
 
 #line default
